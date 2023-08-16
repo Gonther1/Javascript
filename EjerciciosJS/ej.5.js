bea3649f092bd834a1af500b2caa01cc8f5376e9
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-08-16 13:31:24 -0500

    a

---
+++ EjerciciosJS/ej.5.js
@@ -0,0 +1,14 @@
+// ####################################
+// Ejercicio 5
+// ####################################
+
+//Se crea una variable de tipo string
+let secuencia=""
+//Creamos un ciclo for
+for (i=1; i<10;i++) {
+    //A la variable string que hicimos en un comienzo le vamos añadiendo los numeros
+    secuencia+=i+" "
+    //Imprimimos el string para que se vaya mostrando progresivamente
+    console.log(secuencia)
+}
+//Desarrollado por: Luis Andrés Alvarez Silva
