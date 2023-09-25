3d3c0b116bb23369511dd64a139ec83f76f0a002
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-09-25 07:46:00 -0500

    feat: :sparkles: Edit elements in domStudy
    @Gonther1

--- Dom/dom.js
+++ Dom/dom.js
@@ -12,3 +12,20 @@ let targetsWithClassGeneral=document.querySelectorAll('.general')
 targetsWithClassGeneral.forEach((elemento) => {
     console.log(elemento)
 })
+
+// Con .style podemos acceder al css desde javascript
+
+byId.style.backgroundColor="red";
+
+byId.style.color="white";
+
+// Generar html desde javascript
+
+// Para crear una nueva etiqueta desde javascript
+
+let element=document.createElement("p")
+
+document.insertBefore(element,bo)
+
+// Better form
+// document.querySelector("body").insertAdjacentHTML('beforeend','<p>Hola mundo</p>')
